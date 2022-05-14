37e2a2065752ae91d8016f52ed0ba94922be43e7
Daniil Barklon <frig.neutron@gmail.com>
2022-05-14 11:56:38 -0400

    created project in stg

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 resource "jira_project" "triage" {
-  key = "TRIAG"
-  name = "Triage"
+  key              = "TRIAG"
+  name             = "Triage"
+  project_type_key = "software"
+  lead_account_id  = var.jira_auth[terraform.workspace].lead_account_id
 }
